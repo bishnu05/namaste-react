480dc5fef1658f39d82b6c8bec8bc734d8cb6036
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2024-01-01 12:47:54 +0530

    commit

--- MyReact/App.js
+++ MyReact/App.js
@@ -13,7 +13,8 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 // connsole.log(heading); //object
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "heading" }, "I'm a an h1 tag"),
@@ -29,31 +30,31 @@ console.log(parent); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
-const Header = () => {
-  return (
-    <>
-      <header className="header">
-        <div className="left">
-          <img
-            src="https://i.pinimg.com/736x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"
-            alt="Logo"
-          />
-        </div>
-        <div className="center">
-          <input
-            className="input"
-            type="text"
-            placeholder="Search anything you want..."
-          />
-          <button type="submit">Submit</button>
-        </div>
-        <div className="right">
-          <img
-            src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png"
-            alt="User Icon"
-          />
-        </div>
-      </header>
-    </>
-  );
-};
+// const Header = () => {
+//   return (
+//     <>
+//       <header className="header">
+//         <div className="left">
+//           <img
+//             src="https://i.pinimg.com/736x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"
+//             alt="Logo"
+//           />
+//         </div>
+//         <div className="center">
+//           <input
+//             className="input"
+//             type="text"
+//             placeholder="Search anything you want..."
+//           />
+//           <button type="submit">Submit</button>
+//         </div>
+//         <div className="right">
+//           <img
+//             src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png"
+//             alt="User Icon"
+//           />
+//         </div>
+//       </header>
+//     </>
+//   );
+// };
